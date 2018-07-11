98952d1e44bf597e2525589280105cab53e15b9d
GiangPT <giangpt@runsystem.net>
2018-07-11 15:24:37 +0700

    update for use control panel api

--- contracts/ProxyControllerLogic_v1.sol
+++ contracts/ProxyControllerLogic_v1.sol
@@ -11,11 +11,11 @@ contract ProxyControllerLogic_v1 is VersionLogic {
         return Organizations(_organizationsAddress).nonces(_addr);
     }
 
-    function createOrganization(address _organizationsAddress, bytes32 _organizationKey, uint _nonce, bytes _clientSign) public {
+    function createOrganization(address _organizationsAddress, bytes32 _organizationKey, uint _nonce, bytes _clientSign) public onlyByVersionContractOrLogic {
         require(Organizations(_organizationsAddress).createOrganizationWithSign(_organizationKey, _nonce, _clientSign));
     }
 
-    function addOrganizationMember(address _organizationsAddress, address _addr, uint _nonce, bytes _clientSign) public {
+    function addOrganizationMember(address _organizationsAddress, address _addr, uint _nonce, bytes _clientSign) public onlyByVersionContractOrLogic {
         require(Organizations(_organizationsAddress).addMemberWithSign(_addr, _nonce, _clientSign));
     }
     
@@ -23,11 +23,11 @@ contract ProxyControllerLogic_v1 is VersionLogic {
         return PersonalInformations(_personalInformationsAddress).nonces(_addr);
     }
 
-    function createPersonalInformation(address _personalInformationsAddress, address _user, bytes32 _dataKey, bytes32 _dataHash, uint _expires, uint _nonce, bytes _clientSign) public {
+    function createPersonalInformation(address _personalInformationsAddress, address _user, bytes32 _dataKey, bytes32 _dataHash, uint _expires, uint _nonce, bytes _clientSign) public onlyByVersionContractOrLogic {
         require(PersonalInformations(_personalInformationsAddress).createWithSign(_user, _dataKey, _dataHash, _expires, _nonce, _clientSign));
     }
 
-    function updatePersonalInformation(address _personalInformationsAddress, address _user, bytes32 _dataKey, bytes32 _dataHash, uint _expires, uint _nonce, bytes _clientSign) public {
+    function updatePersonalInformation(address _personalInformationsAddress, address _user, bytes32 _dataKey, bytes32 _dataHash, uint _expires, uint _nonce, bytes _clientSign) public onlyByVersionContractOrLogic {
         require(PersonalInformations(_personalInformationsAddress).updateWithSign(_user, _dataKey, _dataHash, _expires, _nonce, _clientSign));
     }
 
